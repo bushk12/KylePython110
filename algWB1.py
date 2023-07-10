8fb9b829a865443c350862b9b2c7c4afd30e721b
totallykile <bushk12@mail.lcc.edu>
2023-07-10 12:54:35 -0400

    Create algWB1.py
    Creation of the Alg. Workbench 1.

---
+++ algWB1.py
@@ -0,0 +1,27 @@
+# main function
+
+def main():
+    # turning a list into a string
+    names = ['Einstein', 'Newton', 'Copernicus', 'Kepler']
+
+    # here I am creating a string with the list.
+    string1 = 'Here are some of the greatest minds ever: {}, {}, {}, and {}.'.format(*names)
+
+    print(string1)
+
+    # creating a string
+    string2 = 'Einstein, Newton, Copernicus, Kepler'
+
+    # using the convert function to print out the string
+    print(convert(string2))
+
+
+# creating a function to convert a string of my choice into a function
+def convert(string):
+
+    # splitting the string into a list
+    li = list(string.split(" "))
+    return li
+
+
+main()
