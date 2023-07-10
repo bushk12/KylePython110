6f481f2d48f27ea2a8fe48785c295f4003df2dfc
totallykile <bushk12@mail.lcc.edu>
2023-07-10 12:53:56 -0400

    Update sales.py
    Made changes for the ability to see the exact days. Making it easier to read and execute.

--- sales.py
+++ sales.py
@@ -4,33 +4,32 @@
 def main():
     # make a list of all the sales inputted by the user
     daily_sale = []
-    inputday = []
-    # creates a variable to control the loop
-    again = 'y' or 'Y'
+    total = 0
+    # variables for creating the list
+    mon = float(input(f'Enter Sales for Monday: '))
+    tue = float(input(f'Enter Sales for Tuesday: '))
+    wed = float(input(f'Enter Sales for Wednesday: '))
+    thu = float(input(f'Enter Sales for Thursday: '))
+    fri = float(input(f'Enter Sales for Friday: '))
+    sat = float(input(f'Enter Sales for Saturday: '))
+    sun = float(input(f'Enter Sales for Sunday: '))
 
-    # creating sales numbers
-    while again == 'y':
-        day = input('Enter a Day:')
-        sale = input('Enter Sales Amount:')
+    # create an error loop below
 
-        # append the item to the list
-        daily_sale.append(sale)
-        inputday.append(day)
+    # append the item to the list
 
-        # adding another day
-
-        print('Do you want to add another day?')
-        again = input('y = yes, anything else = no:')
-        # Total sale of all the sales numbers
-
-    # combine both lists into one list with appropriate inputs
-    print('Here are your sales for each day: ')
-    zipped = zip(inputday, daily_sale)
-    total = zip(zipped)
-    print(list(total))
+    daily_sale.append(mon)
+    daily_sale.append(tue)
+    daily_sale.append(wed)
+    daily_sale.append(thu)
+    daily_sale.append(fri)
+    daily_sale.append(sat)
+    daily_sale.append(sun)
 
 
+    # adding the values together
+    for x in daily_sale:
+        total = sum(daily_sale)
 
-
-
+    print(f'The total sales for the week is ${total}.')
 main()
